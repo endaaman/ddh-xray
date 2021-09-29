d8272b71030399fd435c5327731d9d0b6ddc009d
endaaman <endaaman@frontier.hokudai.ac.jp>
2021-09-29 12:28:41 +0900

    re split

--- gbm.py
+++ gbm.py
@@ -9,22 +9,32 @@ import pandas as pd
 
 from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn import metrics
-import lightgbm as lgb
+import lightgbm as org_lgb
+import optuna
+import optuna.integration.lightgbm as opt_lgb
+from sklearn.model_selection import train_test_split
 
 from endaaman import Commander
 
 
-SEED = 42
+optuna.logging.disable_default_handler()
 
 col_target = 'treatment'
-# cols_feature = df.columns.values.tolist()[1:-1] # exclude label, treatment
-# cols_cat = ['sex', 'family_history', 'breech_presentation', 'skin_laterality', 'limb_limitation']
-cols_cat = ['sex', 'breech_presentation']
-cols_val = ['left_alpha', 'right_alpha', 'left_oe', 'right_oe', 'left_a', 'right_a', 'left_b', 'right_b', ]
+# cols_cat = ['sex', 'breech_presentation']
+# cols_val = ['left_alpha', 'right_alpha', 'left_oe', 'right_oe', 'left_a', 'right_a', 'left_b', 'right_b', ]
+
+cols_cat = []
+cols_val = ['sex', 'breech_presentation', 'left_alpha', 'right_alpha', 'left_oe', 'right_oe', 'left_a', 'right_a', 'left_b', 'right_b', ]
 cols_feature = cols_cat + cols_val
 
+
 class GBM(Commander):
-    def get_data(self):
+    def arg_common(self, parser):
+        parser.add_argument('-r', '--test-ratio', type=float)
+        parser.add_argument('-o', '--use-optuna', action='store_true')
+        parser.add_argument('-s', '--seed', type=int, default=42)
+
+    def pre_common(self):
         df = pd.read_excel('data/table.xlsx', index_col=0)
         df_train = df[df['test'] == 0]
         df_test = df[df['test'] == 1]
@@ -34,53 +44,65 @@ class GBM(Commander):
 
         df_train = pd.concat([df_train, df_measure_train], axis=1)
         df_test = pd.concat([df_test, df_measure_test], axis=1)
-        return df_train, df_test
 
-    def run_demo(self, args):
-        df_train, df_test = self.get_data()
-        print(df_test)
-        print(df_train)
+        self.df_all = pd.concat([df_train, df_test])
+        self.df_all.loc[:, cols_cat]= self.df_all[cols_cat].astype('category')
 
-    def run_train(self, args):
-        df_train, df_test = self.get_data()
+        if self.args.test_ratio:
+            df_train, df_test = train_test_split(self.df_all, test_size=self.args.test_ratio, random_state=self.args.seed)
+            # df_train.loc[:, 'test'] = 0
+            # df_test.loc[:, 'test'] = 1
+        self.df_train = df_train
+        self.df_test = df_test
 
-        folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=SEED)
+        self.lgb = opt_lgb if self.args.use_optuna else org_lgb
+
+    def run_demo(self):
+        print(len(self.df_test))
+        print(len(self.df_train))
+
+    def arg_train(self, parser):
+        parser.add_argument('--roc', action='store_true')
+
+    def run_train(self):
+        folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=self.args.seed)
         # 各foldターゲットのラベルの分布がそろうようにする = stratified K fold
-        folds = folds.split(np.arange(len(df_train)), y=df_train[col_target])
+        folds = folds.split(np.arange(len(self.df_train)), y=self.df_train[col_target])
         folds = list(folds)
 
         gbm_params = {
             'objective': 'binary', # 目的->2値分類
             'num_threads': -1,
-            'bagging_seed': SEED,
-            'random_state': SEED,
+            'bagging_seed': self.args.seed,
+            'random_state': self.args.seed,
             'boosting': 'gbdt',
             'metric': 'auc',
             'verbosity': -1,
         }
 
-        preds_valid = np.zeros([len(df_train)], np.float32)
-        preds_test = np.zeros([5, len(df_test)], np.float32)
+        preds_valid = np.zeros([len(self.df_train)], np.float32)
+        preds_test = np.zeros([5, len(self.df_test)], np.float32)
         df_feature_importance = pd.DataFrame()
 
         for fold in range(5):
-            x_train = df_train.iloc[folds[fold][0]][cols_feature]
-            y_train = df_train.iloc[folds[fold][0]][col_target]
-            x_valid = df_train.iloc[folds[fold][1]][cols_feature]
-            y_valid = df_train.iloc[folds[fold][1]][col_target]
-            x_test = df_test[cols_feature]
+            x_train = self.df_train.iloc[folds[fold][0]][cols_feature]
+            y_train = self.df_train.iloc[folds[fold][0]][col_target]
+            x_valid = self.df_train.iloc[folds[fold][1]][cols_feature]
+            y_valid = self.df_train.iloc[folds[fold][1]][col_target]
+            x_test = self.df_test[cols_feature]
 
             print(f'fold: {fold+1}, train: {len(x_train)}, valid: {len(x_valid)}')
-            train_data = lgb.Dataset(x_train, label=y_train, categorical_feature=cols_cat)
-            valid_data = lgb.Dataset(x_valid, label=y_valid, categorical_feature=cols_cat)
+            train_data = self.lgb.Dataset(x_train, label=y_train, categorical_feature=cols_cat)
+            valid_data = self.lgb.Dataset(x_valid, label=y_valid, categorical_feature=cols_cat)
 
-            model = lgb.train(
+            model = self.lgb.train(
                 gbm_params, # モデルのパラメータ
                 train_data, # 学習データ
                 1000, # 学習を繰り返す最大epoch数, epoch = モデルの学習回数
                 valid_sets=[train_data, valid_data], # 検証データ
                 verbose_eval=100, # 100 epoch ごとに経過を表示する
                 early_stopping_rounds=150, # 150epoch続けて検証データのロスが減らなかったら学習を中断する
+                categorical_feature=cols_cat,
             )
 
             preds_valid[folds[fold][1]] = model.predict(x_valid, num_iteration=model.best_iteration) # 検証データに対する予測を実行
@@ -93,8 +115,7 @@ class GBM(Commander):
             tmp['fold'] = fold + 1
             df_feature_importance = pd.concat([df_feature_importance, tmp], axis=0)
 
-
-        score = metrics.roc_auc_score(df_train[col_target], preds_valid)
+        score = metrics.roc_auc_score(self.df_train[col_target], preds_valid)
         print(f'CV AUC: {score:.6f}')
 
         df_tmp = df_feature_importance.groupby('feature').agg('mean').reset_index()
@@ -105,17 +126,15 @@ class GBM(Commander):
         p = 'out/model.txt'
         model.save_model(p)
         print(f'wrote {p}')
+        if self.args.roc:
+            self.draw_roc(model)
 
-    def run_roc(self, args):
-        df_train, df_test = self.get_data()
+    def draw_roc(self, model):
+        x_train = self.df_train[cols_feature]
+        y_train = self.df_train[col_target]
 
-        model = lgb.Booster(model_file='out/model.txt')
-
-        x_train = df_train[cols_feature]
-        y_train = df_train[col_target]
-
-        x_test = df_test[cols_feature]
-        y_test = df_test[col_target]
+        x_test = self.df_test[cols_feature]
+        y_test = self.df_test[col_target]
 
         pred_train = model.predict(x_train, num_iteration=model.best_iteration)
         pred_test = model.predict(x_test, num_iteration=model.best_iteration)
@@ -135,6 +154,14 @@ class GBM(Commander):
         plt.show()
         print(f'wrote {p}')
 
+    def arg_roc(self, parser):
+        parser.add_argument('-w', '--weights', default='out/model.txt')
+
+    def run_roc(self):
+        model = self.lgb.Booster(model_file=self.args.weight)
+        self.draw_roc(model)
+
 
 
 GBM().run()
+
