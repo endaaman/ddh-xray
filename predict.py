328dcf705e6ce9ef9ba7c824d997e8af47715eb6
endaaman <endaaman@frontier.hokudai.ac.jp>
2021-10-12 19:39:48 +0900

    update

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import os
 import json
+from glob import glob
 
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
@@ -12,28 +13,111 @@ from effdet import EfficientDet, DetBenchPredict, get_efficientdet_config
 from effdet.efficientdet import HeadNet
 
 from augmentation import ResizeAugmentation
-# from datasets import XrayDataset
+from datasets import XRBBDataset
 from utils import pil_to_tensor
 
 from endaaman import TorchCommander
 
+
+SIZE_BY_NETWORK= {
+    'd0': 512,
+    'd1': 640,
+}
+
+LABEL_TO_STR = {
+    1: 'right top',
+    2: 'right out',
+    3: 'right in',
+    4: 'left top',
+    5: 'left out',
+    6: 'left in',
+}
+
 class Predictor(TorchCommander):
-    def create_model(self, network):
-        cfg = get_efficientdet_config(f'tf_efficientdet_{network}')
+    def create_bench(self, state):
+        n = state['args'].network
+        cfg = get_efficientdet_config(f'tf_efficientdet_{n}')
         cfg.num_classes = 6
         model = EfficientDet(cfg, pretrained_backbone=True)
-        # model.class_net = HeadNet(cfg, num_outputs=cfg.num_classes)
-        return model
+        model.load_state_dict(state['state_dict'])
+        bench = DetBenchPredict(model).to(self.device)
+        bench.eval()
+        return bench
 
-    def run_test(self):
-        model = self.create_model('d0')
-        bench = DetBenchPredict(model)
-        images = torch.randn(2, 3, 512, 512)
-        results = bench(images)
+    def pre_common(self):
+        self.font = ImageFont.truetype('/usr/share/fonts/ubuntu/Ubuntu-R.ttf', size=16)
 
-        preds_boxes = preds_boxes.type(torch.LongTensor)
-        # print(preds_boxes)
-        # print(results.shape)
+    def arg_common(self, parser):
+        parser.add_argument('-b', '--batch-size', type=int, default=16)
+
+    def arg_dir(self, parser):
+        parser.add_argument('-w', '--weights', type=str, required=True)
+        parser.add_argument('-i', '--input-dir', type=str, required=True)
+        parser.add_argument('-o', '--output-dir', type=str, default='tmp')
+
+        parser.add_argument('--workers', type=int, default=os.cpu_count()//2)
+
+    def run_dir(self):
+        os.makedirs(self.args.output_dir, exist_ok=True)
+
+        state = torch.load(self.args.weights, map_location=lambda storage, loc: storage)
+        bench = self.create_bench(state)
+
+        image_size = SIZE_BY_NETWORK.get(state['args'].network, 512)
+        imgs = []
+        names = []
+        for p in glob(os.path.join(self.args.input_dir, '*')):
+            imgs.append(Image.open(p))
+            names.append(os.path.basename(p))
+
+        if len(imgs) < 1:
+            print('empty')
+            return
+        results = self.detect_images(bench, imgs, image_size)
+
+        for name, result in tqdm(zip(names, results)):
+            p = os.path.join(self.args.output_dir, name)
+            result.save(p)
+        print('done')
+
+    def detect_images(self, bench, imgs, size=512):
+        imgs = [i.resize((size, size)) for i in imgs]
+
+        bs = self.args.batch_size
+        outputs = []
+        start = 0
+        t = tqdm(range(0, len(imgs), bs))
+        for start in t:
+            batch = imgs[start:start + bs]
+
+            tt = torch.stack([pil_to_tensor(i) for i in batch]).to(self.device)
+            with torch.no_grad():
+                output_tensor = bench(tt)
+            outputs.append(output_tensor.detach().cpu())
+            t.set_description(f'{start} ~ {start + bs} / {len(imgs)}')
+            t.refresh()
+        outputs = torch.cat(outputs).type(torch.long)
+
+        results = []
+        print(outputs.shape)
+        for img, bboxes in zip(imgs, outputs):
+            best_bboxes = []
+            for i in LABEL_TO_STR.keys():
+                m = bboxes[bboxes[:, 5] == i]
+                if len(m) > 0:
+                    best_bboxes.append(m[0])
+                else:
+                    print('missing {LABEL_TO_STR[i]}')
+
+            draw = ImageDraw.Draw(img)
+            for i, result in enumerate(best_bboxes):
+                bbox = result[:4]
+                label = result[5].item()
+                draw.text((bbox[0], bbox[1]), LABEL_TO_STR[label], font=self.font, fill='yellow')
+                draw.rectangle(((bbox[0], bbox[1]), (bbox[2], bbox[3])), outline='yellow', width=1)
+            results.append(img)
+        return results
+
 
     def arg_single(self, parser):
         parser.add_argument('-w', '--weights', type=str, required=True)
@@ -42,48 +126,13 @@ class Predictor(TorchCommander):
 
     def run_single(self):
         dest_dir = self.args.output_dir
-        if not os.path.isdir(dest_dir):
-            print(f'{output_base} is not directory')
-            exit(1)
-
+        os.makedirs(dest_dir, exist_ok=True)
         state = torch.load(self.args.weights, map_location=lambda storage, loc: storage)
-        model = self.create_model(state['args'].network)
-        model.load_state_dict(state['state_dict'])
-        bench = DetBenchPredict(model).to(self.device)
-
+        bench = self.create_bench(state)
 
         img_size = 512
         img = Image.open(self.args.input)
-        img = img.resize((img_size, img_size))
-        img_tensor = pil_to_tensor(img)
-        # img_tensor = img_tensor.permute([0, 2, 1])
-        img_tensor = img_tensor[None, :, :, :]
-
-        results = bench(img_tensor.to(self.device))
-        results = results[0].type(torch.long)
-        bboxes = []
-        bbox_scores = []
-        label_to_str = {
-            1: 'left top',
-            2: 'left out',
-            3: 'left in',
-            4: 'right top',
-            5: 'right out',
-            6: 'right in',
-        }
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype('/usr/share/fonts/ubuntu/Ubuntu-R.ttf', size=16)
-        boxes = []
-        for i in label_to_str.keys():
-            m = results[results[:, 5] == i]
-            if len(m) > 0:
-                boxes.append(m[0])
-        for i, result in enumerate(boxes):
-            bbox = result[:4]
-            label = result[5].item()
-            draw.text((bbox[0], bbox[1]), label_to_str[label], font=font, fill='yellow')
-            draw.rectangle(((bbox[0], bbox[1]), (bbox[2], bbox[3])), outline='yellow', width=1)
-
+        img = self.detect_images(bench, [img])[0]
 
         # model_id = state['args'].network + '_e' + str(state['epoch'])
         # dest_dir = os.path.join(self.args.dest, model_id)
