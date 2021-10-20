11c27e0d86a47482449b751dc0fdf3324eebe5c1
endaaman <endaaman@frontier.hokudai.ac.jp>
2021-10-21 08:23:52 +0900

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
-python train.py effdet -d d4 -b 2 -e 100
-python train.py effdet -d d3 -b 4 -e 100
-python train.py effdet -d d2 -b 8 -e 100
-python train.py effdet -d d1 -b 8 -e 100
-python train.py effdet -d d0 -b 8 -e 100
-python train.py yolo -b 8 -e 100
+python train.py effdet -d d4 -b 2 -e 100 -p 20
+python train.py effdet -d d3 -b 4 -e 100 -p 20
+python train.py effdet -d d2 -b 8 -e 100 -p 20
+python train.py effdet -d d1 -b 8 -e 100 -p 20
+python train.py effdet -d d0 -b 8 -e 100 -p 20
+python train.py yolo -b 8 -e 100 -p 20
