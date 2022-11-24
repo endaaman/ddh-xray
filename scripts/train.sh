2dce8d8ba309181cd749857649f98aec37e92f12
endaaman <endaaman@frontier.hokudai.ac.jp>
2022-11-25 00:45:25 +0900

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
+python cnn.py effdet -d d4 -b 2 -e 200 --save-period 50 --no-show-fig
+python cnn.py effdet -d d3 -b 4 -e 200 --save-period 50 --no-show-fig
+python cnn.py effdet -d d2 -b 8 -e 200 --save-period 50 --no-show-fig
+python cnn.py effdet -d d1 -b 8 -e 200 --save-period 50 --no-show-fig
 # python train.py effdet -d d0 -b 8 -e 200 --save-period 50
-python cnn.py effdet -d d1 -b 8 -e 200 --save-period 50
-python cnn.py effdet -d d2 -b 8 -e 200 --save-period 50
-python cnn.py effdet -d d3 -b 4 -e 200 --save-period 50
-python cnn.py effdet -d d4 -b 2 -e 200 --save-period 50
 # python train.py yolo -b 8 -e 100 -p 20
